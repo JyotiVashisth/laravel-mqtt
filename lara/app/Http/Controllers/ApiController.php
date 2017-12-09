05bc42b119567a95c1d277042fa73bb77eb707da
Jyoti Vashist <jyotivashisth95@gmail.com>
2017-12-09 19:23:13 +0530

    Update ApiController.php

--- lara/app/Http/Controllers/ApiController.php
+++ lara/app/Http/Controllers/ApiController.php
@@ -11,10 +11,6 @@ class ApiController extends Controller
     public function index(Request $request, Client_messages $cm){
         
         $bodyContent = $request->getContent();
-        //inserts message in the database.
-   //   $cm->insert(['message'=>'hello']);  
-        //return "message stored";
-       // return $bodyContent;
         $cm->api($request,$cm);
         return gettype($bodyContent); 
     }
